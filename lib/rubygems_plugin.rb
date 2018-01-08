d4b4e7ddf544d186d5586555906d786c0b4179dd
John Labovitz <johnl@johnlabovitz.com>
2018-01-08 08:51:13 -0600

    Fix require.

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -2,6 +2,6 @@ require 'rubygems/command_manager'
 require 'rubygems/command'
 
 require 'gem-diff/diff_command'
-require 'gem-diff/diff_command/version'
+require 'gem-diff/version'
 
 Gem::CommandManager.instance.register_command(:diff)
